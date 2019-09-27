743a7710e6acae9a64af2c392b16ade6cee27cd5
Lenn Drijkoningen <lenn.drijkoningen@ordina.be>
2019-09-27 09:58:32 +0200

    Make AI smarter & Add game state

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { isNullOrUndefined } from 'util';
 
 interface Cell {
   x: number;
@@ -6,6 +7,12 @@ interface Cell {
   hit?: boolean;
   miss?: boolean;
   type?: string;
+}
+
+interface Enemy {
+  targetStack: Cell[];
+  mode: 'HUNT' | 'KILL';
+  targetList: Cell[];
 }
 
 @Component({
@@ -16,10 +23,14 @@ interface Cell {
 export class GameComponent implements OnInit {
   constructor() {}
 
+  gameState: 'PLACEMENT' | 'COMBAT' | 'GAMEOVER';
+
   playerGrid: Cell[][];
   enemyGrid: Cell[][];
   startCell: Cell;
   endCell: Cell;
+
+  enemy: Enemy;
 
   ships = [
     { type: 'carrier', size: 5, placed: false },
@@ -29,8 +40,14 @@ export class GameComponent implements OnInit {
     { type: 'destroyer', size: 2, placed: false }
   ];
   ngOnInit() {
+    this.gameState = 'PLACEMENT';
     this.enemyGrid = [];
     this.playerGrid = [];
+    this.enemy = {
+      targetStack: [],
+      mode: 'HUNT',
+      targetList: []
+    };
 
     for (let y = 0; y < 10; y++) {
       if (!this.enemyGrid[y]) {
@@ -42,6 +59,10 @@ export class GameComponent implements OnInit {
         this.enemyGrid[y].push({ x, y });
       }
     }
+  }
+
+  message(text: string) {
+    console.log('MESSAGE: ' + text);
   }
 
   copy(element: any): any {
@@ -102,11 +123,10 @@ export class GameComponent implements OnInit {
         });
         ship.placed = true;
       }
-    }
-
-    if (!this.ships.find(s => !s.placed)) {
-      console.log('all ships placed');
-      this.placeEnemyShips();
+      if (!this.ships.find(s => !s.placed)) {
+        console.log('all ships placed');
+        this.placeEnemyShips();
+      }
     }
   }
 
@@ -121,22 +141,120 @@ export class GameComponent implements OnInit {
   }
 
   playerFire(cell: Cell) {
-    this.fire(this.enemyGrid, cell);
-
-    this.enemyFire();
-  }
-
-  enemyFire() {
-    const x1 = Math.round(Math.random() * 10) % 9;
-    const y1 = Math.round(Math.random() * 10) % 9;
-    const cell = { x: x1, y: y1 };
-
-    console.log('firing at ', cell);
-    if (!this.fire(this.playerGrid, cell)) {
-      this.enemyFire;
+    switch (this.gameState) {
+      case 'PLACEMENT':
+        this.message('Place your ships before firing!');
+        break;
+      case 'COMBAT':
+        this.fire(this.enemyGrid, cell);
+        this.enemyFire();
+        break;
+      case 'GAMEOVER':
+        this.message('The game is over!');
+        break;
+      default:
+        break;
     }
   }
 
+  enemyFire() {
+    switch (this.enemy.mode) {
+      case 'HUNT':
+        let parity = 1;
+        let x = 0;
+        let y = 0;
+        while (parity !== 0) {
+          x = Math.round(Math.random() * 10) % 9;
+          y = Math.round(Math.random() * 10) % 9;
+
+          parity = (x + y) % 2;
+        }
+
+        const cell = { x, y };
+
+        if (this.enemy.targetList.find(c => c.x === x && c.y === y)) {
+          this.enemyFire();
+          return;
+        }
+
+        this.enemy.targetList.push(cell);
+
+        console.log('firing at ', cell);
+        if (this.fire(this.playerGrid, cell)) {
+          // Hit => go into 'KILL' mode
+          console.log('KILL MODE');
+
+          this.enemy.mode = 'KILL';
+          // Add surrounding cells to the targetStack
+          this.enemy.targetStack = [
+            ...this.enemy.targetStack,
+            ...this.getSurroundingCells(cell, this.playerGrid)
+          ];
+        } else {
+          // Miss => better luck next time!
+        }
+        break;
+
+      case 'KILL':
+        console.log(this.enemy.targetStack);
+
+        if (this.enemy.targetStack.length > 0) {
+          const cell = this.enemy.targetStack.shift();
+          if (this.fire(this.playerGrid, cell)) {
+            // Hit => add surrounding cells to target list
+            this.enemy.targetStack = [
+              ...this.enemy.targetStack,
+              ...this.getSurroundingCells(cell, this.playerGrid)
+            ];
+          } else {
+            // Miss => better luck next time!
+          }
+        } else {
+          this.enemy.mode = 'HUNT';
+          console.log('HUNT MODE');
+
+          this.enemyFire();
+        }
+        break;
+      default:
+        break;
+    }
+  }
+
+  getSurroundingCells(cell: Cell, grid: Cell[][]): Cell[] {
+    const list = [];
+    const x = cell.x;
+    const y = cell.y;
+
+    const c1 = this.findCell(grid, x - 1, y);
+    const c2 = this.findCell(grid, x + 1, y);
+    const c3 = this.findCell(grid, x, y - 1);
+    const c4 = this.findCell(grid, x, y + 1);
+    if (c1) {
+      list.push(c1);
+    }
+    if (c2) {
+      list.push(c2);
+    }
+    if (c3) {
+      list.push(c3);
+    }
+    if (c4) {
+      list.push(c4);
+    }
+
+    return list.filter(
+      c => isNullOrUndefined(c.hit) && isNullOrUndefined(c.miss)
+    );
+  }
+
+  /**
+   *
+   * @param grid The grid to shoot on
+   * @param cell The target Cell to shoot at
+   *
+   * @returns boolean True on Hit, False on Miss
+   */
   fire(grid: Cell[][], cell: Cell): boolean {
     const gridCell = this.findCell(grid, cell.x, cell.y);
     if (gridCell.hit || gridCell.miss) {
@@ -159,11 +277,14 @@ export class GameComponent implements OnInit {
         .find(c => c.type && !c.hit);
       if (!isAnythingAlive) {
         console.log('GAME OVER');
+        this.gameState = 'GAMEOVER';
       }
+
+      return true;
     } else {
       gridCell.miss = true;
+      return false;
     }
-    return true;
   }
 
   placeEnemyShips() {
@@ -223,5 +344,7 @@ export class GameComponent implements OnInit {
         }
       }
     }
+    this.gameState = 'COMBAT';
+    this.message('Combat has started!');
   }
 }
